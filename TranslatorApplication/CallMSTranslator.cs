be55552f9e7709bb48d69ea26dadc4f6f6252c65
Masaki Kato <maskato@microsoft.com>
2016-09-23 11:30:13 +0900

    基本的な翻訳プログラム

---
+++ TranslatorApplication/CallMSTranslator.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace TranslatorApplication
+{
+    public class CallMSTranslator
+    {
+        private string authToken;
+        private string clientId;
+        private string clientSecret;
+        private string baseUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text=";
+        private string translatedText;
+
+        public CallMSTranslator(string clientId, string clientSecret)
+        {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+
+            AdmAuthentication admAuth = new AdmAuthentication(clientId, clientSecret);
+            AdmAccessToken admToken = admAuth.GetAccessToken();
+            authToken = "Bearer " + admToken.access_token;
+        }
+
+        public string TranslateMethod(string originalText, string fromLang, string toLang)
+        {
+            string uri = baseUrl + System.Web.HttpUtility.UrlEncode(originalText) + "&from=" + fromLang + "&to=" + toLang;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.Headers.Add("Authorization", authToken);
+            WebResponse response = null;
+
+            try
+            {
+                response = httpWebRequest.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String"));
+                    translatedText = (string)dcs.ReadObject(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+
+            return translatedText;
+        }
+        public void ProcessWebException(WebException e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            // Obtain detailed error information
+            string strResponse = string.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)e.Response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(responseStream, System.Text.Encoding.ASCII))
+                    {
+                        strResponse = sr.ReadToEnd();
+                    }
+                }
+            }
+            Console.WriteLine("Http status code={0}, error message={1}", e.Status, strResponse);
+        }
+
+    }
+
+    public class AdmAccessToken
+    {
+
+        public string access_token { get; set; }
+
+        public string token_type { get; set; }
+
+        public string expires_in { get; set; }
+
+        public string scope { get; set; }
+    }
+    public class AdmAuthentication
+    {
+        private static readonly string DatamarketAccessUri = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13";
+        private string clientId;
+        private string clientSecret;
+        private string request;
+        private AdmAccessToken token;
+        private Timer accessTokenRenewer;
+        //Access token expires every 10 minutes. Renew it every 9 minutes only.
+        private const int RefreshTokenDuration = 9;
+
+        public AdmAuthentication(string clientId, string clientSecret)
+        {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+
+            //If clientid or client secret has special characters, encode before sending request
+            this.request = string.Format("grant_type=client_credentials&client_id={0}&client_secret={1}&scope=http://api.microsofttranslator.com", HttpUtility.UrlEncode(clientId), HttpUtility.UrlEncode(clientSecret));
+            this.token = HttpPost(DatamarketAccessUri, this.request);
+
+            //renew the token every specified minutes
+            accessTokenRenewer = new Timer(new TimerCallback(OnTokenExpiredCallback), this, TimeSpan.FromMinutes(RefreshTokenDuration), TimeSpan.FromMilliseconds(-1));
+        }
+
+        public AdmAccessToken GetAccessToken()
+        {
+            return this.token;
+        }
+
+        public void RenewAccessToken()
+        {
+            AdmAccessToken newAccessToken = HttpPost(DatamarketAccessUri, this.request);
+            //swap the new token with old one
+            //Note: the swap is thread unsafe
+            this.token = newAccessToken;
+            Console.WriteLine(string.Format("Renewed token for user: {0} is: {1}", this.clientId, this.token.access_token));
+        }
+
+        public void OnTokenExpiredCallback(object stateInfo)
+        {
+            try
+            {
+                RenewAccessToken();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Failed renewing access token. Details: {0}", ex.Message));
+            }
+            finally
+            {
+                try
+                {
+                    accessTokenRenewer.Change(TimeSpan.FromMinutes(RefreshTokenDuration), TimeSpan.FromMilliseconds(-1));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("Failed to reschedule the timer to renew access token. Details: {0}", ex.Message));
+                }
+            }
+        }
+        public AdmAccessToken HttpPost(string DatamarketAccessUri, string requestDetails)
+        {
+            //Prepare OAuth request 
+            WebRequest webRequest = WebRequest.Create(DatamarketAccessUri);
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(requestDetails);
+            webRequest.ContentLength = bytes.Length;
+
+            using (Stream outputStream = webRequest.GetRequestStream())
+            {
+                outputStream.Write(bytes, 0, bytes.Length);
+            }
+
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AdmAccessToken));
+                //Get deserialized object from JSON stream
+                AdmAccessToken token = (AdmAccessToken)serializer.ReadObject(webResponse.GetResponseStream());
+                return token;
+            }
+        }
+    }
+}
---
+++ TranslatorApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace TranslatorApplication
+{
+    class Program
+    {
+        private static string fromLang = "ja";
+        private static string toLang = "en";
+        private static string originalText = "こんにちは！今日はいい天気ですね。";
+        static void Main(string[] args)
+        {
+            {
+                string clientId = ConfigurationManager.AppSettings["clientId"];
+                string clientSecret = ConfigurationManager.AppSettings["clientSecret"]; ;
+
+                CallMSTranslator callMSTranslator = new CallMSTranslator(clientId, clientSecret);
+                Console.WriteLine(callMSTranslator.TranslateMethod(originalText, fromLang, toLang));
+                Console.ReadKey();
+            }
+        }
+    }
+}
